d6fbe78c52e06c4da51f76a15e5e872ea6f18791
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-07-07 17:05:21 +0530

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import "./navbar.css";
+import Logo from "../../assets/logo.png";
+import Degree from "../../assets/degree.svg";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <div className="logo">
+          <img src={Logo} alt="Yakeen" />
+        </div>
+        <div className="browse">
+          <span>BROWSE</span>
+          <KeyboardArrowDownIcon style={{ fontSize: "20px" }} />
+        </div>
+        <div className="search">
+          <SearchIcon
+            style={{
+              fontSize: "23px",
+              color: "var(--clr-blue-900)",
+              opacity: "0.85",
+            }}
+          />
+          <input type="text" placeholder="Try &#34;Data science&#34;" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="programs">
+          <img src={Degree} alt="" />
+          <span>Degree & Certificate Programs</span>
+        </div>
+        <div className="login">Login</div>
+        <div className="signup">Sign Up</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
