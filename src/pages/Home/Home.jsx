16eec8fde258a0f195574e9ccf83d7bab8c9dd44
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-07-17 16:50:24 +0530

    responsive half done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
     <div className={styles.home}>
       <Hero />
       <Courses />
-      <CourseMenu />
-      <FirstJob />
+      {/* <CourseMenu />
+      <FirstJob /> */}
       <Ready />
       <FAQ />
       <About />
